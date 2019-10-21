c18a7c3653337a954e972a2d2ca6ca57d063608b
Jaro <p.jaronsky@gmail.com>
2019-10-21 10:09:03 +0200

    Refactor time measurement

--- pcmax/solution/base/main.cpp
+++ pcmax/solution/base/main.cpp
@@ -34,10 +34,6 @@ string getTimeElapsed(long time1, const string &unit1, long time2 = 0, const str
     return s.str();
 }
 
-string measureTimeElapsed(chrono::duration<long, ratio<1, 1000000000>> diff) {
-
-}
-
 int main() {
     iostream::sync_with_stdio(false);
 
@@ -57,34 +53,33 @@ int main() {
     auto diff = end - begin;
     auto hours = chrono::duration_cast<chrono::hours>(diff).count();
     auto minutes = chrono::duration_cast<chrono::minutes>(diff).count();
-    if (hours == 0) {
-        auto seconds = chrono::duration_cast<chrono::seconds>(diff).count();
-        if (minutes == 0) {
-            auto milliseconds = chrono::duration_cast<chrono::milliseconds>(diff).count();
-            if (seconds == 0) {
-                auto microseconds = chrono::duration_cast<chrono::microseconds>(diff).count();
-                if (milliseconds == 0) {
-                    auto nanoseconds = chrono::duration_cast<chrono::nanoseconds>(diff).count();
-                    if (microseconds == 0) timeElapsed = getTimeElapsed(nanoseconds, "ns");
-                    else {
-                        nanoseconds %= 1000;
-                        timeElapsed = timeElapsed = getTimeElapsed(microseconds, "μs", nanoseconds, "ns");
-                    }
-                } else {
-                    microseconds %= 1000;
-                    timeElapsed = getTimeElapsed(milliseconds, "ms", microseconds, "μs");
-                }
-            } else {
-                milliseconds %= 1000;
-                timeElapsed = getTimeElapsed(seconds, "s", milliseconds, "ms");
-            }
-        } else {
-            seconds %= 60;
-            timeElapsed = getTimeElapsed(minutes, "min", seconds, "s");
-        }
-    } else {
+    if (hours) {
         minutes %= 60;
         timeElapsed = getTimeElapsed(hours, "h", minutes, "min");
+    } else {
+        auto seconds = chrono::duration_cast<chrono::seconds>(diff).count();
+        if (minutes) {
+            seconds %= 60;
+            timeElapsed = getTimeElapsed(minutes, "min", seconds, "s");
+        } else {
+            auto milliseconds = chrono::duration_cast<chrono::milliseconds>(diff).count();
+            if (seconds) {
+                milliseconds %= 1000;
+                timeElapsed = getTimeElapsed(seconds, "s", milliseconds, "ms");
+            } else {
+                auto microseconds = chrono::duration_cast<chrono::microseconds>(diff).count();
+                if (milliseconds) {
+                    microseconds %= 1000;
+                    timeElapsed = getTimeElapsed(milliseconds, "ms", microseconds, "μs");
+                } else {
+                    auto nanoseconds = chrono::duration_cast<chrono::nanoseconds>(diff).count();
+                    if (microseconds) {
+                        nanoseconds %= 1000;
+                        timeElapsed = timeElapsed = getTimeElapsed(microseconds, "μs", nanoseconds, "ns");
+                    } else timeElapsed = getTimeElapsed(nanoseconds, "ns");
+                }
+            }
+        }
     }
 
     cout << "Solution [" << solution << "] found in " << timeElapsed << endl;
